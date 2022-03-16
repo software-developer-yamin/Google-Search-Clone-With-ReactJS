802c8265c6380082884db340a2df7ac3f38b7447
DeveloperYamin <developer.yamin@gmail.com>
2022-03-16 09:57:35 +0600

    used all components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Routes from './components/Routes';
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
+    <section className={darkTheme ? "dark" : ""}>
+      <section className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+        <Navbar />
+        <Routes />
+        <Footer />
+      </section>
+    </section>
   );
 }
 
