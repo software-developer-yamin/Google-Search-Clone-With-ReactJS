89d19b83c4b80da91cf8d1c06071158a17204d9c
DeveloperYamin <developer.yamin@gmail.com>
2022-03-16 14:03:43 +0600

    creating Result Context Provider for API

---
+++ src/contexts/ResultContextProvider.jsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState } from "react";
+
+const ResultContext = createContext();
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+
+export const ResultContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // type = /videos, /images, /search
+  const getResults = async (type) => {
+    setIsLoading(true);
+
+    const response = await fetch(`${baseUrl}${type}`, {
+      method: "GET",
+      headers: {
+        "x-user-agent": "desktop",
+        "x-rapidapi-host": "google-search3.p.rapidapi.com",
+        "x-rapidapi-key": "76bb70a0b8mshe4356a9bcb4211bp177763jsne100735e70f0",
+      },
+    });
+
+    const data = await response.json();
+
+    setResults(data);
+    setIsLoading(false);
+  };
+
+  return (
+    <ResultContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}
+    >
+      {children}
+    </ResultContext.Provider>
+  );
+};
+
+export const useResultContext = () => useContext(ResultContext);
