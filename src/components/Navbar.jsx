efff14501cd8035e9eebfa9b7b8c7c3906f8cd0b
DeveloperYamin <developer.yamin@gmail.com>
2022-03-16 10:10:51 +0600

    Import BrowserRouter from react-router-dom near the top of your file and wrap your app in a <BrowserRouter>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b border-gray-200 dark:border-gray-700">
+      <nav className="flex justify-between items-center space-x-5 w-screen"></nav>
+    </header>
+  );
 }
 
 export default Navbar;
